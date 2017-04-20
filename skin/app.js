9102811656c035781e3d5252180929f9ec104fe7
axepu <axepuff@gmail.com>
2017-04-21 00:01:18 +0300

    v1.0

--- skin/app.js
+++ skin/app.js
@@ -6,7 +6,6 @@ function Arifmetics(arr1, arr2) {
   text.children[0].id = a;
   text.children[1].innerHTML = b;
   text.children[1].id = b;
-  
   function Render(term) {
     this.inputPos = (term*39+50 - 15)/2;
     this.input = document.createElement('input');
@@ -21,49 +20,54 @@ function Arifmetics(arr1, arr2) {
       this.arrow.style.left = a*39 + 35 + 'px';
       edge.appendChild(this.input);
       edge.appendChild(this.arrow);
-    } else if (render2) {
-      console.log('123');
-      this.inpute.style.position = 'relative';
-      text.appendChild(this.input);
-    } else {
+    }  else {
       this.input.style.left = this.inputPos + 'px';
       this.arrow.style.left = '35px';
       edge.appendChild(this.input);
       edge.appendChild(this.arrow);
     }
-    
+    this.accept = function() {
+        this.input.style.color = '#000';
+        this.input.setAttribute('disabled', 'disabled');
+    };
     this.input.focus();
   }
   
   function Check(render, val1, val2) {
-    render.input.oninput = function() {
-      if(render.input.value != val1)  {
-        render.input.style.color = '#f00';
+    var input = render.input || render;
+    console.log(input);
+    input.oninput = function() {
+      if(input.value != val1 && render.input)  {
+        input.style.color = '#f00';
         document.getElementById(val1).style.background = "#ff6000";
-      } else if(val2) {
-        render.input.style.color = '#000';
-        render.input.setAttribute('disabled', 'disabled');
+      } else if(input.value != val1) {
+        input.style.color = '#f00';
+      }
+      else if(val2) {
+        render.accept();
+        input.setAttribute('disabled', 'disabled');
         document.getElementById(val1).style.background = "#fff";
         var render2 = new Render(val2);
         Check(render2, b);
-      } else if(document.getElementById('summ')){
-        render.input.style.color = '#000';
-        render.input.setAttribute('disabled', 'disabled');
+      } else if(!document.getElementById('summ')) {
+          input.style.color = '#000';
+          input.setAttribute('disabled', 'disabled');
+      } else {
+        render.accept();
         document.getElementById(val1).style.background = "#fff";
         document.getElementById('summ').remove();
-        var final = new Render(c);
-        /*var final = document.createElement('input');
+        var final = document.createElement('input');
         final.className = 'edge__summ';
+        final.id = c;
         document.querySelector('.edge__txt').appendChild(final);
-        final.focus();*/
-
-      } else {
-        
+        final.focus();
+        Check(final, c);
       }
   }
 }
   var render1 = new Render(a);
+
   Check(render1, a, b);
 }
 
-Arifmetics([6,9], [11,14])
+Arifmetics([6,9], [11,14]);
